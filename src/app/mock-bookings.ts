97e610a36969327654f3e1542e7ca42c13143345
Yusuf Kara <94810547+yusufkara95@users.noreply.github.com>
2022-06-17 01:20:20 +0200

    Update mock-bookings.ts

--- src/app/mock-bookings.ts
+++ src/app/mock-bookings.ts
@@ -11,14 +11,14 @@ export const Bookings: Booking[] = [
     {
         id : 2,
         name: "Max Mustermann",
-        roomNumber: 50,
+        roomNumber: 500,
         startDate: new Date("2021-07-25"),
         endDate: new Date("2021-08-01")
     },
     {
         id : 3,
         name: "John Doe",
-        roomNumber: 2,
+        roomNumber: 122,
         startDate: new Date("2021-09-05"),
         endDate: new Date("2021-09-19")
     },
