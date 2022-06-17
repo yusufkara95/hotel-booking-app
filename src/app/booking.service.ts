dde0cb1e1259c72afe9fd986798c89b624282db2
Yusuf Kara <94810547+yusufkara95@users.noreply.github.com>
2022-06-17 21:08:57 +0200

    Create the booking service

---
+++ src/app/booking.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Bookings } from './mock-bookings';
+import { Booking } from './booking';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookingService {
+
+  constructor() { }
+
+  getBookings() : Booking[] {
+    return Bookings;
+  }
+
+  deleteBooking(booking: Booking): void {
+    var index = Bookings.indexOf(booking);
+    Bookings.splice(index, 1);
+  }
+
+  getBookingById(id: number) : Booking {
+    var bookingById = Bookings.find(x => x.id == id)!;
+    return bookingById;
+  }
+
+  addBooking(booking: Booking) : void {
+    Bookings.push(booking);
+  }
+
+  updateBooking(booking: Booking) : void {
+    var currentBooking = this.getBookingById(booking.id);
+    currentBooking = booking;
+  }
+}
